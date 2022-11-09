d31817c5b4b4274396c242c7e7b354cff0c0de82
dmytro <3a43mka@gmail.com>
2022-11-10 00:12:25 +0200

    function signatures

---
+++ sem_5_lab_1/lab1/forward_index.py
@@ -0,0 +1,6 @@
+def build_forward_index(docs):
+    return []
+
+
+def search_in_forward_index(index, keyword):
+    return []
---
+++ sem_5_lab_1/lab1/inverted_index.py
@@ -0,0 +1,6 @@
+def build_inverted_index(docs):
+    return []
+
+
+def search_in_inverted_index(index, keyword):
+    return []
